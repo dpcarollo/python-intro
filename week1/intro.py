86ec0477a0f9a94a9a1f21cc22a185223e2d2a91
Dave Carollo <dpcarollo87@gmail.com>
2019-12-04 16:55:00 -0500

    get repo setup

---
+++ week1/intro.py
@@ -0,0 +1,26 @@
+print("Hello World!")
+print("You are a good son")
+print("I really am proud of you")
+print("thank you for teaching me, Senpai")
+print('hello')
+
+#code is sequential, look at the numbers below, x=a will overwrite itself.
+
+a = 1
+b = 2
+x = a
+a = 3
+b = x
+x = a
+
+print(a,b,x)
+
+a = "nice"
+b = "important"
+x = a
+y = b
+b = x
+a = y
+
+print( a + " to be " + b)
+
